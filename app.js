1edd3d8927f0436d4a9d765320b1fc94374cbcc8
Trevor Mearns <spiral272@gmail.com>
2020-01-06 17:55:12 +0900

    js making a lot more sense

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const userScore = 0;
-const computerScore = 0;
-const userScore_Span = document.getElementById("user-score");
-const computerScore_Span = document.getElementById("computer-score");
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
@@ -12,22 +12,53 @@ function getComputerChoice() {
     const choices = ['r', 'p', 's'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
-}
+};
 
-console.log(getComputerChoice());
+function win() {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    console.log("you won dawg! Your score is " + userScore);
+};
+
+function lose() {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    console.log("you lost dawg!");
+};
+
+function tie() {
+    console.log("its a tie dawg!");
+};
+
 function game(userChoice) {
     const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            win();
+            break;
+        case 'rp':
+        case 'ps':
+        case 'sr':
+            lose();
+            break;
+        case 'rr':
+        case 'ss':
+        case 'pp':
+            tie();
+            break;
+    }
     console.log("The computers choice is " + computerChoice);
     console.log("Your choice is " + userChoice);
 }
 
 function main() {
-rock_div.addEventListener("click", () => game("r"));
+    rock_div.addEventListener("click", () => game("r"));
 
-paper_div.addEventListener("click", () => game("p"));
+    paper_div.addEventListener("click", () => game("p"));
 
-scissors_div.addEventListener("click", () => game("s"));
-
+    scissors_div.addEventListener("click", () => game("s"));
 };
 
 main();
